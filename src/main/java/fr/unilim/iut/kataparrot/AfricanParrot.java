a6049a3d500a14d55a99b3a8fe98dbbef0e3a9f9
Cathy Descoutures <cathy.descoutures@etu.unilim.fr>
2024-09-30 14:32:11 +0200

    add AfricanParrot

---
+++ src/main/java/fr/unilim/iut/kataparrot/AfricanParrot.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.kataparrot;
+
+public class AfricanParrot extends Parrot{
+
+	public AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
+		super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, voltage, isNailed);
+	}
+
+	@Override
+	public double getSpeed() {
+		return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+	}
+}
