6a7e25c2287fa5a3d0db9bd281f51aa46e7afb54
Cathy Descoutures <cathy.descoutures@etu.unilim.fr>
2024-09-30 14:27:04 +0200

    add EuropeanParrot

---
+++ src/main/java/fr/unilim/iut/kataparrot/EuropeanParrot.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.kataparrot;
+
+public class EuropeanParrot extends Parrot{
+
+	public EuropeanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
+		super(ParrotTypeEnum.EUROPEAN, numberOfCoconuts, voltage, isNailed);
+	}
+
+	@Override
+	public double getSpeed() {
+		return getBaseSpeed();
+	}
+}
